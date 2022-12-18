1f8a097fa8806d84d616a75e691ae4d26cdcc683
Tchinonso <lightersmercury@gmail.com>
2022-12-18 13:16:14 +0100

    new react portfolio project started

--- src/index.js
+++ src/index.js
@@ -2,25 +2,30 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
-//import App from "./components/App"
-//import "./styles/global.css"
-//import App from "./components/GoodUse/App"
-//import App from "./components/UseStateArray/App"
-//import App from "./components/UseStateCounter/App"
-//import App from "./components/UseEffect/App";
+// import App from "./components/App"
+// import "./styles/global.css"
+// import App from "./components/GoodUse/App"
+// import App from "./components/UseStateArray/App"
+// import App from "./components/UseStateCounter/App"
+// import App from "./components/UseEffect/App";
 // import App from "./components/image-slider-carousel-with-ReactJs/App"
-import App from "./components/ReactRoute/App";
+// import App from "./components/ReactRoute/App";
+import App from './App'
+import GlobalStyles from "./styles/GlobalStyle";
+import Typography from './styles/Typography'
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	
+
 	<StrictMode>
 		<BrowserRouter>
+			<GlobalStyles />
+			<Typography />
 			<App />
 		</BrowserRouter>
 	</StrictMode>
-	
+
 );
