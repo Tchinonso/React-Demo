5fc899fb11f9f7fcdd17f4979ca5b02861e227d3
Tchinonso <lightersmercury@gmail.com>
2023-01-03 20:44:38 +0100

    fixed

---
+++ src/NewComponent/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Layout.scss'
+import SideBar from './SideBar/SideBar'
+
+const Layout = () => {
+  return (
+	<div>
+	  <SideBar />
+	</div>
+  )
+}
+
+export default Layout
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import './index.css'
+// import { BrowserRouter } from "react-router-dom";
+// import './index.css'
 // import App from "./components/App"
 // import "./styles/global.css"
 // import App from "./components/GoodUse/App"
@@ -9,26 +10,29 @@ import './index.css'
 // import App from "./components/UseEffect/App";
 // import App from "./components/image-slider-carousel-with-ReactJs/App"
 // import App from "./components/ReactRoute/App";
-// import App from './components/Portfolio/App'
-// import GlobalStyles from "./components/Portfolio/styles/GlobalStyle";
-// import Typography from './components/Portfolio/styles/Typography'
-import App from "./Komponents/SimpleNavKomponent/App"
+import App from './components/Portfolio/App'
+import GlobalStyles from "./components/Portfolio/styles/GlobalStyle";
+import Typography from './components/Portfolio/styles/Typography'
+// import App from "./Komponents/SimpleNavKomponent/App"
+// import App from "./NewComponent/App";
 
 
 
-
-// ReactDOM.render(
-//   <>
-//     <GlobalStyles />
-//     <Typography />
-//     <App />
-// 	</>,
-// 	document.getElementById('root')
-// );
 
 ReactDOM.render(
   <>
+    <GlobalStyles />
+    <Typography />
     <App />
-  </>,
-  document.getElementById("root")
+	</>,
+	document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
